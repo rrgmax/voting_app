797bd8f675ffb546f829029886b12fee8027565c
Rogerio <rrgmax@bol.com.br>
2019-02-18 20:12:15 -0300

    Fix a type error in the Choice class and run migrations

--- votings/models.py
+++ votings/models.py
@@ -6,6 +6,6 @@ class Question(models.Model):
     pub_date = models.DateTimeField('Published date')
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, no_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
     chioce_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
