3ddc97111264318d1cac79af3d395d39b7572918
Rogerio <rrgmax@bol.com.br>
2019-02-18 20:09:45 -0300

    Set Question and Choice classes into models.py file

--- votings/models.py
+++ votings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('Published date')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, no_delete = models.CASCADE)
+    chioce_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
