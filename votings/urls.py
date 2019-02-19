9bfa772d2b8df576f92616073c4c58316860634c
Rogerio <rrgmax@bol.com.br>
2019-02-18 21:35:55 -0300

    Update votings/urls.py and index.html.py

--- votings/urls.py
+++ votings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'votings'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
