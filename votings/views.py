61de7d4fd086781f5e6466cba82adfa96c70a72e
Rogerio <rrgmax@bol.com.br>
2019-02-18 23:37:02 -0300

    Update views.py file

--- votings/views.py
+++ votings/views.py
@@ -27,6 +27,7 @@ def vote(request, question_id):
             'question': question,
             'error_message': "You didn't select a choice.",
         })
+
     else:
         selected_choice.votes += 1
         selected_choice.save()
