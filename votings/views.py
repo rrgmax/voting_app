5fa98c1d985f018945a2949af690fdb1b04a8a8d
Rogerio <rrgmax@bol.com.br>
2019-02-18 22:03:34 -0300

    Update results function into views.py file

--- votings/views.py
+++ votings/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.url import reverse
 
 # Create your views here.
 from. models import Question
@@ -13,8 +15,20 @@ def detail(request, question_id):
     return render(request, 'votings/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'votings/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'votings/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('votings:results', args=(question.id,)))
+
